2ad20a3070f3fe771e945ec93d2cc510e1366b77
Davydikmi_PC <davydovnikita9@gmail.com>
2024-03-24 13:47:40 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.cpp
@@ -0,0 +1,25 @@
+#include<string>
+#include<vector>
+#include <vcclr.h>
+#include <msclr/marshal_cppstd.h>
+#include <msclr\marshal.h>
+#include <msclr\marshal_windows.h>
+#include <nlohmann/json.hpp>
+
+
+#include "MainWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThreadAttribute]
+int main(array<String^>^ args)
+{
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	PasswordManager::MainWindow window;
+	Application::Run(% window);
+
+}
+
---
+++ MainWindow.h
@@ -0,0 +1,66 @@
+#pragma once
+
+namespace PasswordManager {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MainWindow
+	/// </summary>
+	public ref class MainWindow : public System::Windows::Forms::Form
+	{
+	public:
+		MainWindow(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MainWindow()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(493, 459);
+			this->Name = L"MainWindow";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"ћенеджер паролей";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
